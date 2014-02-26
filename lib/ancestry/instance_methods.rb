c9deecc5c92a6183cfb8198dad8ce2a5cad77fcb
Christoffer Winterkvist <chris@zenangst.com>
2014-02-26 10:47:28 +0100

    Removed exception in child_ancestry

--- lib/ancestry/instance_methods.rb
+++ lib/ancestry/instance_methods.rb
@@ -85,8 +85,7 @@ module Ancestry
 
     # The ancestry value for this record's children
     def child_ancestry
-      # New records cannot have children
-      raise Ancestry::AncestryException.new('No child ancestry for new record. Save record before performing tree operations.') if new_record?
+      if new_record?; return nil; end
 
       if self.send("#{self.ancestry_base_class.ancestry_column}_was").blank? then id.to_s else "#{self.send "#{self.ancestry_base_class.ancestry_column}_was"}/#{id}" end
     end
